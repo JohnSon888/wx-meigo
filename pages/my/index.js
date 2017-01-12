a30af194b4986fe201fcbe7683036157765f6cd6
sunqiangqiang <sunqiangqiang@shopex.cn>
2017-01-12 23:09:13 +0800

    修改错误

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,14 +1,6 @@
-var fav = require('../../utils/fav.js');
 
 Page({
     data: {},
     onLoad: function() {},
-    onShow: function() {
-        this.setData({
-            favlist: fav.getFavList()
-        })
-    },
-    playTap: function(e) {
-
-    }
+    
 })
