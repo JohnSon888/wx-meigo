6a1ff15a9a92aa002821ad90f3c0ea80413fc5ae
sunqiangqiang <sunqiangqiang@shopex.cn>
2017-10-21 13:31:32 +0800

    首页修改

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,7 +3,14 @@ Page({
   data: {
     singlePinItem: [],
     widgets: [],
+    subWidgets: [],
     currentMenuIdx: '0', //当前点击的索引
+    currentId: '0', //当前列表的索引
+    isShow: false,
+    isLinkShow: false,
+    imgW: '0', //商品列表图的宽度
+    blockImgH: '0',//商品块状图高度
+    subWidgetsList: [], // 每个栏目的商品列表
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
@@ -30,22 +37,50 @@ Page({
     })
   },
   onLoad: function() {
+    var screenW = wx.getSystemInfoSync().screenWidth;
     api.get('http://m.meigooo.com/api/npages/200003')
       .then(res => {
         console.log(res)
         var resData = res.data.body;
         var singleItem = resData.widgets[8].contents.shift();
+        var imgW = (screenW - 3) + 'rpx'; 
         // 过滤商品
         this.setData({
+          isShow: true,
           singlePinItem: singleItem,
-          widgets: resData.widgets
+          widgets: resData.widgets,
+          imgW: imgW
         });
-      })    
+      }) 
   },
   handleMenuTap: function(e) {
-    console.log(e.currentTarget.dataset);
+    var contentId = e.currentTarget.dataset.target;
+    // 屏幕宽度
+    var screenW = wx.getSystemInfoSync().screenWidth;
     this.setData({
-      currentMenuIdx: e.currentTarget.dataset.idx
+      currentMenuIdx: e.currentTarget.dataset.idx,
+      currentId: e.currentTarget.dataset.idx
     })
+    if (contentId){
+      api.get('http://m.meigooo.com/api/npages/' + contentId)
+        .then(res => {
+          console.log(res)
+          var resData = res.data.body;
+          var subWidgetsList = [];
+          for (var i in resData.widgets) {
+            if (resData.widgets[i].widgetType == '31') {
+              subWidgetsList = resData.widgets[i];
+            }
+          }
+          // 过滤商品
+          this.setData({
+            subWidgets: resData.widgets,
+            blockImgH: screenW / 2 + 'rpx',
+            isLinkShow: true,
+            subWidgetsList: subWidgetsList
+
+          });
+        }) 
+    }
   }
 })
