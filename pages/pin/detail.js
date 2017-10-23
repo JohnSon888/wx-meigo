a00dced1c51dfcdb4f21792a4bbf56a46e92998f
sunqiangqiang <sunqiangqiang@shopex.cn>
2017-10-23 23:08:54 +0800

    商品详情页

---
+++ pages/pin/detail.js
@@ -0,0 +1,31 @@
+Page({
+  data: {
+    swipers:[],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  onLoad: function () { },
+
+})
