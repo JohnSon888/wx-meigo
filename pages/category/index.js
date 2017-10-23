c71f9c1fbede09715fcba7b0796b8f56be264937
sunqiangqiang <sunqiangqiang@shopex.cn>
2017-10-23 14:20:13 +0800

    分类

--- pages/category/index.js
+++ pages/category/index.js
@@ -2,53 +2,52 @@ var api = require('../../utils/api.js')
 Page({
   data: {
     text: "Page main",
-    background: [
-      {
-        color: 'green',
-        sort: 1
-      },
-      {
-        color: 'red',
-        sort: 2
-      },
-      {
-        color: 'yellow',
-        sort: 3
-      }
-    ],
     indicatorDots: true,
     vertical: false,
     autoplay: false,
     interval: 3000,
     duration: 1200,
-    toView: 'blue',
+    scrollTop: 100,
     selectedMenuId: '1',
     currentMenuId: '0',
+    toView: 'red',
     rightSideW: '0',
     cateList: [],
-    total: {
-      count: 0,
-      money: 0
-    }
+    menuListH: '0' // 列表高度
+  },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
   },
   selectMenu: function (event) {
     let data = event.currentTarget.dataset
+    console.log(event);
+    
     this.setData({
-      toView: data.tag,
       selectedMenuId: data.id,
-      currentMenuId: data.idx
+      currentMenuId: data.idx,
+      toView: data.tag
     })
-    // this.data.toView = 'red'
+    
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     
-    var res = wx.getSystemInfoSync()
-    var rw = (res.screenWidth - 90) * 2 + 'rpx';
-    console.log(res.screenWidth);
-    console.log(rw);
+    var sys = wx.getSystemInfoSync()
+    var rw = (sys.screenWidth - 90) * 2 + 'rpx';
+    
+    console.log(sys.screenWidth);
+
+    console.log(sys.screenHeight);
+    var menuListH = sys.screenHeight*2 + 'rpx';
     this.setData({
-      rightSideW: rw
+      rightSideW: rw,
+      menuListH: menuListH
     })
 
     api.get('http://m.meigooo.com/category/getBoutiqueList')
